09b0f87ef558c2d3807ec0fbe9f1d4124982405a
isaki <10616227+isaki@users.noreply.github.com>
2025-07-17 20:27:31 -0400

    Made profile.d script have strict compatibility with SH.

--- profile.d/dotnet-cli-telemetry-opt-out.sh
+++ profile.d/dotnet-cli-telemetry-opt-out.sh
@@ -1,3 +1,4 @@
 # README: Place this file in /etc/profile.d
 # https://aka.ms/dotnet-cli-telemetry
-export DOTNET_CLI_TELEMETRY_OPTOUT=1
+DOTNET_CLI_TELEMETRY_OPTOUT=1
+export DOTNET_CLI_TELEMETRY_OPTOUT
